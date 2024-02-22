c7917d636758cc873cd6270a4b02584f1d970afb
RiyaZingade <riya.zingade@gmail.com>
2024-02-22 15:36:06 -0800

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Riya Zingade on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
