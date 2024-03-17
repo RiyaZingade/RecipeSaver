edb4bf992843140e6df0f11f1c34a19f18bf1082
RiyaZingade <riya.zingade@gmail.com>
2024-03-17 01:45:32 -0700

    finished, need to debug recipe card page

---
+++ RecipeSaver/Views/Components/RecipeList.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeList.swift
+//  RecipeSaver
+//
+//  Created by Riya Zingade on 3/17/24.
+//
+
+import SwiftUI
+
+struct RecipeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeList()
+}
